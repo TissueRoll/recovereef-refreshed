0e1de90aab2024647c397e81a8671e470d6977cb
TissueRoll <christopher.dizon@protonmail.com>
2021-02-08 17:47:50 +0800

    hooked everything to be tested on itch

---
+++ Assets/Scripts/MainMenu/LevelSelector.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.MainMenu
+{
+	public class LevelSelector : MonoBehaviour
+	{
+
+		public void SelectLevel(int level)
+		{
+			switch (level)
+			{
+				case 1:
+					SceneManager.LoadScene("01_00_Game_Easy");
+					break;
+				case 2:
+					SceneManager.LoadScene("01_01_Game_Medium");
+					break;
+				case 3:
+					SceneManager.LoadScene("01_02_Game_Hard");
+					break;
+				default:
+					Debug.Log("Unknown Level");
+					break;
+			}
+		}
+	}
+}
--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -5,6 +5,40 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+	public void getLevel(int level)
+	{
+		switch (level)
+		{
+			case 1:
+				SceneManager.LoadScene("01_00_Game_Easy");
+				break;
+			case 2:
+				SceneManager.LoadScene("01_01_Game_Medium");
+				break;
+			case 3:
+				SceneManager.LoadScene("01_02_Game_Hard");
+				break;
+			case 4:
+				SceneManager.LoadScene("00_00_Game_Tutorial");
+				break;
+			case 5:
+				SceneManager.LoadScene("00_01_Game_Tutorial");
+				break;
+			case 6:
+				SceneManager.LoadScene("00_02_Game_Tutorial");
+				break;
+			case 7:
+				SceneManager.LoadScene("00_03_Game_Tutorial");
+				break;
+			case 8:
+				SceneManager.LoadScene("00_04_Game_Tutorial");
+				break;
+			default:
+				Debug.Log("Unknown Level. Exiting");
+				getExit();
+				break;
+		}
+	}
 	public void getNewGame()
 	{
 		// build index method
