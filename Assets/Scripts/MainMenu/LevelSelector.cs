2b65524a1e2d922442022e2a503176e3e3566820
TissueRoll <christopher.dizon@protonmail.com>
2021-03-04 15:39:15 +0800

    finished base tutorial and built the v0.03 game

--- Assets/Scripts/MainMenu/LevelSelector.cs
+++ Assets/Scripts/MainMenu/LevelSelector.cs
@@ -20,6 +20,21 @@ namespace Assets.Scripts.MainMenu
 				case 3:
 					SceneManager.LoadScene("01_02_Game_Hard");
 					break;
+				case 4:
+					SceneManager.LoadScene("00_00_Tutorial_Controls");
+					break;
+				case 5:
+					SceneManager.LoadScene("00_01_Tutorial_Corals_Basics");
+					break;
+				case 6:
+					SceneManager.LoadScene("00_02_Tutorial_Substrata_Algae");
+					break;
+				case 7:
+					SceneManager.LoadScene("00_03_Tutorial_Events");
+					break;
+				case 8:
+					SceneManager.LoadScene("00_04_Tutorial_Corals_Advanced");
+					break;
 				default:
 					Debug.Log("Unknown Level");
 					break;
